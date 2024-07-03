3be9ecdb5146b2dfc9f4a1b591ed65a773da60ce
Xnay74 <muhammadrinasyah475@gmail.com>
2024-07-03 17:38:54 +0700

    commit pertama

---
+++ Cat.java
@@ -0,0 +1,29 @@
+package PBO;
+
+public class Cat {
+    public String nama = "Zidane";
+    public int umur = 2;
+    public String gender = "cowok";
+    public String makanan = "whiskas";
+    public String warna = "hitam putih";
+    public void iscathungry(){
+        System.out.println(this.nama + " is hungry");
+    }
+    public void iscatitred(){
+        System.out.println(this.nama + " go to sleep");
+    }
+    public void iscatangry(){
+        System.out.println(this.nama + " want to eat");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ MainDriver.java
@@ -0,0 +1,12 @@
+package PBO;
+
+public class MainDriver {
+    public static void main(String[] args) {
+        Cat Cat1 = new Cat();
+        Cat1.iscathungry();
+        Cat1.iscatitred();
+        Cat1.iscatangry();
+    }
+}
+
+
